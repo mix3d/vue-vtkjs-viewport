ccb06a3212e3433998b3e9a9e794a6e69479d44b
Madison Dickson <madison@Madisons-MacBook-Pro.local>
2019-09-23 17:58:51 -0700

    add externals because we're going to add things back!

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,18 @@ module.exports = {
   chainWebpack: config => {
     config.externals({
       'vtk.js': 'vtk.js',
+      'cornerstone-core': {
+        commonjs: 'cornerstone-core',
+        commonjs2: 'cornerstone-core',
+        amd: 'cornerstone-core',
+        root: 'cornerstone',
+      },
+      'cornerstone-math': {
+        commonjs: 'cornerstone-math',
+        commonjs2: 'cornerstone-math',
+        amd: 'cornerstone-math',
+        root: 'cornerstoneMath',
+      }
     });
     // Shader Loader
     config.module
