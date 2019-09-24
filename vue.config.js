6daba5259f8e22e4d945e1b2aa777ca468be5656
Madison Dickson <madison@Madisons-MacBook-Pro.local>
2019-09-23 19:57:42 -0700

    stop bundling polyfills, add webpack analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,37 @@
 // const path = require("path");
 // const CopyPlugin = require("copy-webpack-plugin");
 
+const plugins = []
+if(process.env.NODE_ENV === "production") {
+  const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+  plugins.push(new BundleAnalyzerPlugin())
+}
+
+const vtkRegex = /\b(vtk.js)/
+
 //prettier-ignore
 module.exports = {
+  configureWebpack: {
+    plugins,
+  },
   chainWebpack: config => {
     config.externals([
-      /\b(vtk.js)/,
+      vtkRegex,
       {
-        'cornerstone-core': {
-          commonjs: 'cornerstone-core',
-          commonjs2: 'cornerstone-core',
-          amd: 'cornerstone-core',
-          root: 'cornerstone',
+        "cornerstone-core": {
+          commonjs: "cornerstone-core",
+          commonjs2: "cornerstone-core",
+          amd: "cornerstone-core",
+          root: "cornerstone"
         },
-        'cornerstone-math': {
-          commonjs: 'cornerstone-math',
-          commonjs2: 'cornerstone-math',
-          amd: 'cornerstone-math',
-          root: 'cornerstoneMath',
+        "cornerstone-math": {
+          commonjs: "cornerstone-math",
+          commonjs2: "cornerstone-math",
+          amd: "cornerstone-math",
+          root: "cornerstoneMath"
         }
-      },
+      }
     ]);
     // Shader Loader
     config.module
