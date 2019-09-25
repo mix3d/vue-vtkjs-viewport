6513052e10e2a6b9ddfae05d0c52d1023af9db7c
Madison Dickson <madison@Madisons-MacBook-Pro.local>
2019-09-25 11:38:33 -0700

    new async loadImageData, courtesy of @nathanreyes

--- src/library/lib/loadImageData.js
+++ src/library/lib/loadImageData.js
@@ -1,9 +1,10 @@
-import { loadAndCacheImage } from "cornerstone-core";
+import cornerstone from "cornerstone-core";
 import getSliceIndex from "./data/getSliceIndex.js";
 import insertSlice from "./data/insertSlice.js";
+import throttle from "./throttle";
 
 function loadImageDataProgressively(imageIds, imageData, metaDataMap, zAxis) {
-  const loadImagePromises = imageIds.map(loadAndCacheImage);
+  const loadImagePromises = imageIds.map(cornerstone.loadAndCacheImage);
 
   const insertPixelData = image => {
     const { imagePositionPatient } = metaDataMap.get(image.imageId);
@@ -27,13 +28,50 @@ function loadImageDataProgressively(imageIds, imageData, metaDataMap, zAxis) {
   //return loadImagePromises[0];
   return Promise.all(loadImagePromises);
 }
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-export default function loadImageData(imageDataObject) {
+async function loadImageDataProgressivelyAsync(
+  imageIds,
+  imageData,
+  metaDataMap,
+  zAxis,
+  onProgressCB
+) {
+  const msProgress = 50;
+  const msLoad = 5;
+  const imageCount = imageIds.length;
+  const throttleUpdateProgress = throttle(onProgressCB, 50);
+
+  for (let idx = 0; idx < imageCount; idx++) {
+    const imageId = imageIds[idx];
+    const image = await cornerstone.loadAndCacheImage(imageId);
+    const { imagePositionPatient } = metaDataMap.get(imageId);
+    const sliceIndex = getSliceIndex(zAxis, imagePositionPatient);
+    const pixels = image.getPixelData();
+    insertSlice(imageData, pixels, sliceIndex);
+    throttleUpdateProgress((idx + 1) / imageCount);
+    await wait(idx < imageCount - 1 ? msLoad : msProgress);
+  }
+}
+
+export function loadImageData(imageDataObject) {
   return loadImageDataProgressively(
     imageDataObject.imageIds,
     imageDataObject.vtkImageData,
     imageDataObject.metaDataMap,
     imageDataObject.zAxis
+  ).then(() => {
+    imageDataObject.loaded = true;
+  });
+}
+
+export default function loadImageDataAsync(imageDataObject, onProgressCB) {
+  return loadImageDataProgressivelyAsync(
+    imageDataObject.imageIds,
+    imageDataObject.vtkImageData,
+    imageDataObject.metaDataMap,
+    imageDataObject.zAxis,
+    onProgressCB
   ).then(() => {
     imageDataObject.loaded = true;
   });
